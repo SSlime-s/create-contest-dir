b9242238ff539414e5a038a3b5e1ea813df2c33e
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-07-29 11:56:42 +0900

    ✨ enum で場合分けするように

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod templates;
 
-use std::{fs, io::Write};
+use std::{
+    fs::{self, OpenOptions},
+    io::{Read, Seek, SeekFrom, Write},
+    process::Command,
+};
 
-use crate::templates::{CHILD_FILE_TEMPLATE, MAIN_FILE_TEMPLATE};
+use crate::templates::{CARGO_TOML, CHILD_FILE_TEMPLATE, MAIN_FILE_TEMPLATE};
 
 enum ErrorMessages {
     FailedCreateDir,
@@ -19,35 +23,101 @@ impl ErrorMessages {
     }
 }
 
+enum Contests {
+    ABC,
+    ARC,
+    AGC,
+}
+impl Contests {
+    fn value(&self) -> &'static str {
+        match *self {
+            Contests::ABC => "abc",
+            Contests::ARC => "arc",
+            Contests::AGC => "agc",
+        }
+    }
+
+    fn problem_names(&self) -> Vec<String> {
+        let a_to_f = ('a'..='f').map(|x| x.to_string()).collect::<Vec<String>>();
+        let a_to_h = ('a'..='h').map(|x| x.to_string()).collect::<Vec<String>>();
+        match *self {
+            Contests::ABC => a_to_h,
+            Contests::ARC => a_to_f,
+            Contests::AGC => a_to_f,
+        }
+    }
+}
+
 fn main() {
-    fs::create_dir("abc-210").expect(ErrorMessages::FailedCreateDir.value());
-    fs::create_dir("abc-210/src").expect(ErrorMessages::FailedCreateDir.value());
-    let mut main_file = fs::File::create("abc-210/src/main.rs").expect(ErrorMessages::FailedCreateFile.value());
-    main_file.write_all(MAIN_FILE_TEMPLATE
-        .replace("{{mods}}", (0..6)
-            .map(|x| (('a' as u8 + x) as char).to_string())
-            .map(|x| format!("mod {};", x))
-            .collect::<Vec<String>>()
-            .join("\n")
-            .as_str())
-        .replace("{{programs}}", (0..6)
-            .map(|x| ('a' as u8 + x) as char)
-            .collect::<String>()
-            .as_str())
-        .replace("{{mains}}", (0..6)
-            .map(|x| (('a' as u8 + x) as char).to_string())
-            .map(|x| format!("        \"{}\" => crate::{}::main(),", x, x))
-            .collect::<Vec<String>>()
-            .join("\n")
-            .as_str())
-        .trim_start()
-        .as_bytes())
+    Command::new("cargo")
+        .args(&["new", "--bin", "abc-210"])
+        .output()
+        .expect(ErrorMessages::FailedCreateDir.value());
+    let mut main_file =
+        fs::File::create("abc-210/src/main.rs").expect(ErrorMessages::FailedCreateFile.value());
+    let contest_name = "abc-210";
+    let contest_type = Contests::ABC;
+    main_file
+        .write_all(
+            MAIN_FILE_TEMPLATE
+                .replace(
+                    "{{mods}}",
+                    contest_type
+                        .problem_names()
+                        .into_iter()
+                        .map(|x| format!("mod {};", x))
+                        .collect::<Vec<String>>()
+                        .join("\n")
+                        .as_str(),
+                )
+                .replace(
+                    "{{programs}}",
+                    contest_type.problem_names().join("").as_str(),
+                )
+                .replace(
+                    "{{mains}}",
+                    contest_type
+                        .problem_names()
+                        .into_iter()
+                        .map(|x| format!("        \"{}\" => crate::{}::main(),", x, x))
+                        .collect::<Vec<String>>()
+                        .join("\n")
+                        .as_str(),
+                )
+                .trim_start()
+                .as_bytes(),
+        )
         .expect(ErrorMessages::FailedWrite.value());
-    for x in 'a'..='f' {
-        let mut child_file = fs::File::create(format!("abc-210/src/{}.rs", x)).expect("failed to create file");
-        child_file.write_all(CHILD_FILE_TEMPLATE
-            .trim_start()
-            .as_bytes())
+
+    for x in contest_type.problem_names() {
+        let mut child_file =
+            fs::File::create(format!("{}/src/{}.rs", contest_name ,x)).expect("failed to create file");
+        child_file
+            .write_all(CHILD_FILE_TEMPLATE.trim_start().as_bytes())
             .expect(ErrorMessages::FailedWrite.value());
     }
+
+    let mut cargo_toml = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(format!("{}/Cargo.toml", contest_name))
+        .expect(ErrorMessages::FailedCreateFile.value());
+    let mut contents = String::new();
+    cargo_toml
+        .read_to_string(&mut contents)
+        .expect(ErrorMessages::FailedWrite.value());
+    cargo_toml
+        .set_len(0)
+        .expect(ErrorMessages::FailedWrite.value());
+    cargo_toml
+        .seek(SeekFrom::Start(0))
+        .expect(ErrorMessages::FailedWrite.value());
+    cargo_toml
+        .write_all(
+            contents
+                .trim_start()
+                .replace("[dependencies]", CARGO_TOML)
+                .as_bytes(),
+        )
+        .expect(ErrorMessages::FailedWrite.value());
 }
