8232401f97e0e3b882dfc063bb222dacf63767f3
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-08-06 12:50:54 +0900

    ♻️ 改行ちょっときれいにした

--- src/get_request.rs
+++ src/get_request.rs
@@ -8,11 +8,9 @@ async fn get_text(url: &str) -> Result<String, Error> {
 pub async fn get_cargo_toml() -> Result<String, Error> {
     get_text("https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled/Cargo.toml").await
 }
-
 pub async fn get_cargo_lock() -> Result<String, Error> {
     get_text("https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled/Cargo.lock").await
 }
-
 pub async fn get_rust_toolchain() -> Result<String, Error> {
     get_text("https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled/rust-toolchain").await
 }
