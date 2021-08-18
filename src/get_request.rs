34e24aa493681183511154fd3f92d79b1857504c
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-08-19 01:31:41 +0900

    ♻️ `rust-lang-ja` からとってくるところをさらにまとめた

--- src/get_request.rs
+++ src/get_request.rs
@@ -1,14 +1,14 @@
-async fn get_text(url: &str) -> Result<String, reqwest::Error> {
-    let s = reqwest::get(url).await?.text().await?;
-    Ok(s)
+const JA_ALL_ENABLED: &'static str = "https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled";
+async fn get_ja_all_enabled_text(file_path: &str) -> Result<String, reqwest::Error> {
+    reqwest::get(&format!("{}/{}", JA_ALL_ENABLED, file_path)).await?.text().await
 }
 
 pub async fn get_cargo_toml() -> Result<String, reqwest::Error> {
-    get_text("https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled/Cargo.toml").await
+    get_ja_all_enabled_text("Cargo.toml").await
 }
 pub async fn get_cargo_lock() -> Result<String, reqwest::Error> {
-    get_text("https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled/Cargo.lock").await
+    get_ja_all_enabled_text("Cargo.lock").await
 }
 pub async fn get_rust_toolchain() -> Result<String, reqwest::Error> {
-    get_text("https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled/rust-toolchain").await
+    get_ja_all_enabled_text("rust-toolchain").await
 }
