ccd9eee0d4e7c4d63be492e7bf71a80f3a528b88
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-08-21 23:18:01 +0900

    👕 `cargo fmt`

--- src/utils/fetch_files.rs
+++ src/utils/fetch_files.rs
@@ -1,6 +1,10 @@
-const JA_ALL_ENABLED: &'static str = "https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled";
+const JA_ALL_ENABLED: &'static str =
+    "https://raw.githubusercontent.com/rust-lang-ja/atcoder-rust-base/ja-all-enabled";
 async fn get_ja_all_enabled_text(file_path: &str) -> Result<String, reqwest::Error> {
-    reqwest::get(&format!("{}/{}", JA_ALL_ENABLED, file_path)).await?.text().await
+    reqwest::get(&format!("{}/{}", JA_ALL_ENABLED, file_path))
+        .await?
+        .text()
+        .await
 }
 
 pub async fn get_cargo_toml() -> Result<String, reqwest::Error> {
